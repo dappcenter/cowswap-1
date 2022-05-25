01b05b9a045164755de076554234507abeac5ab2
Anxo Rodriguez <anxolin@gmail.com>
2022-05-25 18:21:53 +0100

    quick test analytics

--- src/components/analytics/GoogleAnalyticsReporter.tsx
+++ src/components/analytics/GoogleAnalyticsReporter.tsx
@@ -1,18 +1,19 @@
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import { useEffect } from 'react'
-import ReactGA from 'react-ga'
+import ReactGA from 'react-ga4'
 import { RouteComponentProps } from 'react-router-dom'
 import { getCLS, getFCP, getFID, getLCP, Metric } from 'web-vitals'
 
 import { GOOGLE_ANALYTICS_CLIENT_ID_STORAGE_KEY } from './index'
 
 function reportWebVitals({ name, delta, id }: Metric) {
-  ReactGA.timing({
-    category: 'Web Vitals',
-    variable: name,
-    value: Math.round(name === 'CLS' ? delta * 1000 : delta),
-    label: id,
-  })
+  // TODO: Fix
+  // ReactGA.timing({
+  //   category: 'Web Vitals',
+  //   variable: name,
+  //   value: Math.round(name === 'CLS' ? delta * 1000 : delta),
+  //   label: id,
+  // })
 }
 
 // tracks web vitals and pageviews
--- src/custom/components/analytics/GoogleAnalyticsReporter.tsx
+++ src/custom/components/analytics/GoogleAnalyticsReporter.tsx
@@ -1,18 +1,19 @@
 import { useActiveWeb3React } from 'hooks/web3'
 import { useEffect } from 'react'
-import ReactGA from 'react-ga'
+import ReactGA from 'react-ga4'
 import { RouteComponentProps } from 'react-router-dom'
 import { getCLS, getFCP, getFID, getLCP, Metric } from 'web-vitals'
 
 const NETWORK_DIMENSION = 'dimension1'
 
 function reportWebVitals({ name, delta, id }: Metric) {
-  ReactGA.timing({
-    category: 'Web Vitals',
-    variable: name,
-    value: Math.round(name === 'CLS' ? delta * 1000 : delta),
-    label: id,
-  })
+  // // TODO: Fix
+  // ReactGA.timing({
+  //   category: 'Web Vitals',
+  //   variable: name,
+  //   value: Math.round(name === 'CLS' ? delta * 1000 : delta),
+  //   label: id,
+  // })
 }
 
 export default function GoogleAnalyticsReporter({ location: { pathname, search } }: RouteComponentProps): null {
